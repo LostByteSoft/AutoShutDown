fa942b706ac2a09fdb2f765ba8701b54d6be54ac
LostByteSoft <mad_mike_41@hotmail.com>
2017-10-02 10:55:36 -0400

    Delete INS AutoShutDown.bat

--- INS AutoShutDown.bat
+++
@@ -1,8 +0,0 @@
-@taskkill /f /im "AutoShutDown.exe"
-copy "AutoShutDown.exe" "C:\Program Files\"
-copy "AutoShutDown.ini" "C:\Program Files\"
-copy "*.ico" "C:\Program Files\"
-copy "*.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs\"
-"C:\Program Files\AutoShutDown.exe"
-@pause
-@exit
